c005aa7df48c10fdfe7765d8cd4bc954a945b2e3
stormiarias <sanduagaarias@gmail.com>
2016-07-07 12:36:21 -0700

    initial commit

---
+++ FooBar/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FooBar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("foobar");
+
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("foo");
+
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("bar");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
